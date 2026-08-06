12cfd6ad4ceed02c33a2acbaff201f875da06d32
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-08-06 11:39:40 -0300

    writable signals e computed signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { Produto } from '../produto/produto';
 
 @Component({
@@ -8,21 +8,31 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  //writable signal - signal (reativo) que permite alterações (com set ou update)
   produtos = signal([
     { nome: 'Notebook', preco: 3800 },
     { nome: 'Mouse', preco: 179 },
     { nome: 'Fone', preco: 80 },
   ]);
 
+  // computed signal - observa outro signal e se atualiza automaticamente
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  }); //computed signal - esse calcula o valor total dos produtos
+
   exibirProduto(nome: string) {
     console.log('Produto selecionado:', nome);
     // Aqui você pode atualizar o estado, abrir modal, etc.
   }
-
+  // adiciona um item ao writable signal - o upted é pra adicionar algo novo
   adicionarProduto() {
-    this.produtos.update((listaAtual) => [
-      ...listaAtual,
-       { nome: 'Teclado', preco: 250 }
-      ]);
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Teclado', preco: 250 }]);  
   }
+  //altera um item ao writable signal - o set é pra alterar o que já existe
+  substituirProdutos() {
+    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
+  }
+  
 }
