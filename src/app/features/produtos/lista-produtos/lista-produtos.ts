8e02251dc4bf29cb559d74a0d72912c4bc1590d2
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-08-06 10:19:23 -0300

    Atualizar estrutura do projeto e signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Produto } from '../produto/produto';
 
 @Component({
@@ -8,14 +8,21 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  produtos = [
+  produtos = signal([
     { nome: 'Notebook', preco: 3800 },
     { nome: 'Mouse', preco: 179 },
     { nome: 'Fone', preco: 80 },
-  ];
+  ]);
 
   exibirProduto(nome: string) {
     console.log('Produto selecionado:', nome);
     // Aqui você pode atualizar o estado, abrir modal, etc.
+  }
+
+  adicionarProduto() {
+    this.produtos.update((listaAtual) => [
+      ...listaAtual,
+       { nome: 'Teclado', preco: 250 }
+      ]);
   }
 }
