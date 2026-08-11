40c2c3a201ddfbda970a95fa710a4f69983e97ff
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-08-11 09:44:45 -0300

    Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { Routes } from '@angular/router';
-import { Home } from './features/home/home/home';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
-
 export const routes: Routes = [
   {
     path: '',
-    component: Home,
+    loadComponent: () => import('./features/home/home/home').then((m) => m.Home),
   },
   {
     path: 'produtos',
-    component: ListaProdutos,
+    loadComponent: () =>
+      import('./features/produtos/lista-produtos/lista-produtos').then((m) => m.ListaProdutos),
   },
   {
     path: 'carrinho',
-    component: Carrinho,
+    loadComponent: () => import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
