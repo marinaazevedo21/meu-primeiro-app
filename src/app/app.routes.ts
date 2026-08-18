cc399b81f1b0dc63931fdfa062170bd5ca743726
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-08-18 10:11:47 -0300

    Guards Intercetos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { authGuard } from './core/auth.guard';
+import { adminGuard } from './core/admin.guard';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -24,10 +25,14 @@ export const routes: Routes = [
     loadComponent: () => import('./features/checkout/checkout/checkout').then((m) => m.Checkout),
   },
   {
+    path: 'admin',
+    canActivate: [adminGuard],
+    loadComponent: () => import('./features/admin/admin/admin').then((m) => m.Admin),
+  },
+  {
     path: 'login',
     loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
   },
-
   {
     path: '**',
     redirectTo: '',
