e81ec3d005ae0917b1be397d2cc2f8568abd7a2c
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-08-18 09:06:43 -0300

    Formulario de Checkout

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
   ReactiveFormsModule,
   FormGroup,
@@ -25,17 +25,34 @@ export class Checkout {
     endereco: new FormControl('', [Validators.required, Validators.minLength(5)]),
   });
 
+  compraFinalizada = signal(false);
+
   finalizar() {
+    this.compraFinalizada.set(false);
+
+    if (this.carrinhoService.carrinhoVazio()) {
+      console.log('Não é possível finalizar uma compra com o carrinho vazio.');
+      return;
+    }
+
     if (this.formulario.invalid) {
       console.log('Formulário inválido');
+      this.formulario.markAllAsTouched();
       return;
     }
 
     const dados = this.formulario.value;
     const itens = this.carrinhoService.itens();
+    const total = this.carrinhoService.total();
 
+    console.log('Compra finalizada com sucesso!');
     console.log('Dados do formulário:', dados);
     console.log('Itens do carrinho:', itens);
+    console.log('Total da compra:', total);
+
+    this.carrinhoService.limpar();
+    this.formulario.reset();
+    this.compraFinalizada.set(true);
   }
 }
 function nomeSemNumeros(control: AbstractControl): ValidationErrors | null {
