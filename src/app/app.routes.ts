6e0cc4ded091e8188049cffed9fbbf34d5a97167
Marina Azevedo <franciscaazevedo011@gmail.com>
2026-08-19 09:05:14 -0300

    Controle de Acesso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -30,6 +30,12 @@ export const routes: Routes = [
     loadComponent: () => import('./features/admin/admin/admin').then((m) => m.Admin),
   },
   {
+    path: 'acesso-negado',
+    loadComponent: () =>
+      import('./features/acesso-negado/acesso-negado/acesso-negado').then((m) => m.AcessoNegado),
+  },
+
+  {
     path: 'login',
     loadComponent: () => import('./features/login/login/login').then((m) => m.Login),
   },
